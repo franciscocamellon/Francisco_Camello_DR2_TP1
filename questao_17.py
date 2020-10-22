a88708e331dcfec6df2286fb25e1354cb7875e18
franciscocamellon <franciscocamellon@gmail.com>
2020-10-22 18:23:09 -0300

    Initial code

--- questao_17.py
+++ questao_17.py
@@ -19,11 +19,22 @@ class Questao_17():
         """
         Constructor
         """
+        self.input = int
+        self.data = {}
+        self.num = int
 
     def funcname(self):
         """
         Docstring
         """
+        size = []
+        while len(size) < 3:
+            self.input = int(input('Digite uma string: '))
+            size.append(self.input)
+        size.append(size[0])
+        self.data = tuple(size)
+
+        return print(self.data)
         return
 
     def print_result(self):
